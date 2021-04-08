da74a81d93bab1b7ae7929b93297da69bf0413cf
jennifer tracy <jenniferetracy@gmail.com>
2021-04-08 13:08:32 -0400

    first commit, react router, header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./Home";
+import Portfolio from "./Portfolio";
+import About from "./About";
+import Header from "./Header";
+import styled from "styled-components";
 
-function App() {
+
+
+ function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+       <Header/>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+     
+    </Router>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,64 @@
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
+
+
+function Header() {
+    const history = useHistory()
+
+    const Siteheader =styled.header`
+    overflow: hidden;
+    background-color: #001e57;
+    padding: 10px 10px;
+    color: #78ebbd
+    `
+
+    const Ulnav = styled.ul`
+    list-style-type: none;
+    float: right
+    `;
+
+    const Linav = styled.li`
+    display: inline;
+    padding: 1rem;
+    
+
+    `;
+    const H1 = styled.h1`
+    display: inline;
+    padding-left: 4rem
+
+    `
+    const Link = styled.a`
+    color: #78ebbd;
+    text-decoration: none;
+    padding-top: .5rem;
+    
+
+    :hover {
+        background-color: #78ebbd;
+        color: black;
+       
+      }
+    `
+
+    return (
+        <Siteheader> 
+            <H1>Jen Tracy</H1>
+           
+                <Ulnav>
+                <Linav>
+                    <Link onClick={() =>history.push("/")}> Home </Link>
+                </Linav>
+                <Linav>
+                    <Link onClick={() =>history.push("/about")}>About</Link>
+                </Linav>
+                <Linav>
+                    <Link onClick={() =>history.push("/portfolio")}>Portfolio</Link>
+                </Linav>
+                </Ulnav>
+           
+        </Siteheader> 
+    )
+  }
+
+export default Header 
